7cbce5eb35fc79897d2ddf8fc1481409cd9d8605
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2008-07-10 20:58:46 +0000

    2nd try: create model: consists of a World,  which manages a collection of Rooms World creates one room on startup Rooms are not rendered yet

--- src/view/renderer.py
+++ src/view/renderer.py
@@ -1,16 +1,17 @@
 from pyglet import clock
-from pyglet.gl import *
+from pyglet.gl import glClear, glClearColor, GL_COLOR_BUFFER_BIT
 
 
 class Renderer(object):
 
     def __init__(self, window):
         self.window = window
-        glClearColor(0.0, 0.0, 1.0, 1.0)
         self.clockDisplay = clock.ClockDisplay()
+        self.clearColor = (0.0, 0.0, 1.0, 1.0)
 
 
     def draw(self):
+        glClearColor(*self.clearColor)
         glClear(GL_COLOR_BUFFER_BIT)
         self.clockDisplay.draw()
 
