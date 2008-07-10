ef9f8e660c9a75d0966c7a7d3dd7497c5106bcf0
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2008-07-10 03:59:38 +0000

    create package for model. fix blooper in testcase.py. rename acceptance_tests.

---
+++ trunk/src/model/tests/world_test.py
@@ -0,0 +1,13 @@
+import fix_pythonpath
+
+from testutils.testcase import MyTestCase, run_test
+
+
+class World_test(MyTestCase):
+
+    def testConstructor(self):
+        self.fail("not done")
+
+
+if __name__ == "__main__":
+    run_test()
--- trunk/src/run_all_tests.py
+++ trunk/src/run_all_tests.py
@@ -3,25 +3,24 @@ from unittest import TextTestRunner
 
 from testutils.testcase import combine
 
+from acceptancetests.tests.acceptancetest_test import AcceptanceTest_test
+from controller.tests.gameloop_test import Gameloop_test
+from model.tests.world_test import World_test
 from testutils.tests.testcase_test import TestCase_test
-
-from acceptance_tests.tests.acceptance_test_test import AcceptanceTest_test
+from view.tests.renderer_test import Renderer_test
 
 from acceptance_tests.AT001_app_opens_a_fullscreen_window import \
     AT001_app_opens_a_fullscreen_window
-
-from controller.tests.gameloop_test import Gameloop_test
-from view.tests.renderer_test import Renderer_test
 
 
 def run_all_tests():
     runner = TextTestRunner(verbosity=2)
     suite = combine(
-        TestCase_test,
         AcceptanceTest_test,
-        AT001_app_opens_a_fullscreen_window,
+        TestCase_test,
         Gameloop_test,
         Renderer_test,
+        AT001_app_opens_a_fullscreen_window,
     )
     runner.run(suite)
 
