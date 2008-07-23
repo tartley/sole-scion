c71b2c0752662a9d0741994731a2e67076284e48
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2008-07-23 04:10:54 +0000

    Add testutils.listener

---
+++ trunk/src/testutils/listener.py
@@ -0,0 +1,36 @@
+from copy import deepcopy
+
+class Listener(object):
+
+    def __init__(self):
+        self.reset()
+        self.returnValue = None
+        self.returnValueList = []
+
+    def reset(self):
+        self.argsList = []
+        self.kwargsList = []
+
+    triggered = property(lambda self: len(self.argsList) > 0)
+    triggerCount = property(lambda self: len(self.argsList))
+
+    def _getArgs(self):
+        if self.triggered:
+            return self.argsList[-1]
+
+    args = property(_getArgs)
+
+    def _getKwargs(self):
+        if self.triggered:
+            return self.kwargsList[-1]
+
+    kwargs = property(_getKwargs)
+
+    def __call__(self, *args, **kwargs):
+        self.argsList.append(deepcopy(args))
+        self.kwargsList.append(kwargs)
+        if self.returnValueList:
+            return self.returnValueList.pop(0)
+        else:
+            return self.returnValue
+
