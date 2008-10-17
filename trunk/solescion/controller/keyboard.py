de8c47fd1640eb54d9cd64feed4ccd799893c69c
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2008-10-17 09:59:14 +0000

    Quickie knock-together of a 'thrust' style spaceship with which the player can navigate the game world, pushing objects around, etc.

--- trunk/solescion/controller/keyboard.py
+++ trunk/solescion/controller/keyboard.py
@@ -1,13 +1,18 @@
 # pylint: disable-msg=E0611
 #   No name 'key' in module '_ModuleProxy'. No idea what this is.
 
-from pyglet.window.key import modifiers_string, symbol_string
+from pyglet.window.key import KeyStateHandler, modifiers_string, symbol_string
+
+
+keystate = KeyStateHandler()
 
 handlers = {
 }
+
 
 def on_key_press(symbol, modifiers):
     if symbol in handlers:
         handlers[symbol]()
     else:
         print "key:", symbol_string(symbol), modifiers_string(modifiers)
+
--- trunk/solescion/model/material.py
+++ trunk/solescion/model/material.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 class Material(object):
 
     def __init__(self, density, elast, frict, color):
@@ -6,11 +8,15 @@ class Material(object):
         self.friction = frict
         self.color = color
 
+crazyWeight = 1/2
+
 # name             density  elasti fricti  color
-gold    = Material(19.0,    0.5,    0.5,   (255, 255,   0))
-granite = Material( 2.7,    0.5,    2.0,   (100,  80,  70))
-rubber  = Material( 1.5,    1.0,   10.0,   ( 20, 130, 200))
-ice     = Material( 0.92,   0.5,    0.1,   (200, 210, 255))
-bamboo  = Material( 0.35,   0.2,    0.4,   (150, 130,  50))
-air     = Material( 0.0013, 0.0,    0.1,   (  0,  50, 100))
+air     = Material( 0.0013 * crazyWeight, 0.0,    0.1,   (  0,  50, 100))
+bamboo  = Material( 0.35 * crazyWeight,   0.2,    0.4,   (150, 130,  50))
+flesh   = Material( 1.5 * crazyWeight,    0.3,    1.0,   (  0, 127,   0))
+gold    = Material(19.0 * crazyWeight,    0.5,    0.5,   (255, 255,   0))
+granite = Material( 2.7 * crazyWeight,    0.5,    2.0,   (100,  80,  70))
+ice     = Material( 0.92 * crazyWeight,   0.5,    0.1,   (200, 210, 255))
+rubber  = Material( 1.5 * crazyWeight,    1.0,   10.0,   ( 20, 130, 200))
+steel   = Material( 7.7,    0.5,    1.0,   ( 90, 150, 200))
 
