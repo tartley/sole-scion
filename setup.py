2fdfcc0eb176b4349144c0e41444e854b508a071
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2008-11-06 15:48:22 +0000

    Fix py2exe setup.py, executable now works. hooray!
    Fix some test includes, to reveal the test failures presumably caused by
    the last hurried checkin of the 'thrust spaceship' code.

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+import os
 from distutils.core import setup
 import py2exe
+import pymunk
+
+pymunk_dir = os.path.dirname(pymunk.__file__)
 
 setup(
     windows=['run.py'],
+    data_files=[
+        ('.', [os.path.join(pymunk_dir, 'chipmunk.dll')]),
+    ],
     options={
         "py2exe":{
             "optimize": 2,
             "excludes": ['email', 'email.Utils'],
         }
-    }
+    },
 )
 
