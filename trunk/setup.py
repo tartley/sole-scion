88354f742d64b9baabb8974c93edbc62a0937b81
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2009-01-09 18:55:38 +0000

    Begin work on setup.py to create source distribution using distutils. Spruce the TODO.txt

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,20 +1,62 @@
-from os.path import dirname, join
 from distutils.core import setup
-import py2exe
+from os.path import dirname, join
+import platform
+
 import pymunk
 
-chipmunk_dll = join(dirname(pymunk.__file__), 'chipmunk.dll')
+is_windows = platform.system() in ['Microsoft', 'Windows']
+if is_windows:
+    import py2exe
 
-setup(
-    windows=['run.py'],
-    data_files=[
-        ('.', [chipmunk_dll]),
-    ],
-    options={
-        "py2exe":{
-            "optimize": 2,
-            "excludes": ['email', 'email.Utils'],
-        }
-    },
-)
+
+def getargs():
+
+    args = dict(
+        name='solescion',
+        version='0.2.2dev',
+        url='http://code.google.com/p/sole-scion/',
+        author='Jonathan Hartley',
+        author_email='tartley@tartley.com',
+
+        py_modules = ['run', 'run_tests'],
+        packages = [
+            'pyglet',
+            'pymunk',
+            'solescion',
+            'solescion.acceptancetests',
+            'solescion.acceptancetests.tests',
+            'solescion.controller',
+            'solescion.controller.tests',
+            'solescion.model',
+            'solescion.model.tests',
+            'solescion.model.shards',
+            'solescion.model.shards.tests',
+            'solescion.testutils',
+            'solescion.testutils.tests',
+            'solescion.utils',
+            'solescion.utils.tests',
+            'solescion.view',
+            'solescion.view.tests',
+        ],
+    )
+    win_args = dict(
+        windows=['run.py'],
+        options={
+            "py2exe":{
+                "optimize": 2,
+                "excludes": ['email', 'email.Utils'],
+            }
+        },
+        data_files=[
+            ('.', [join(dirname(pymunk.__file__), 'chipmunk.dll')]),
+        ],
+    )
+
+    if is_windows:
+        args.update(win_args)
+
+    return args
+
+
+setup(**getargs())
 
