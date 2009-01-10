6a629c18f5622b6c6c2e1955df275e2f7ea1f836
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2009-01-10 19:11:46 +0000

    split hastily added 'player' class into its own module

--- solescion/model/world.py
+++ solescion/model/world.py
@@ -1,20 +1,14 @@
 from __future__ import division
 from math import pi, sin, cos
 
-from pyglet.window import key
 from pymunk import Body, inf, init_pymunk, Space
 
-from solescion.controller.keyboard import Keyboard
 from solescion.model.chunk import Chunk
 from solescion.model.material import Material
+from solescion.model.player import Player
 from solescion.model.room import Room
 from solescion.model.shards.block import Block
 from solescion.model.shards.disc import Disc
-
-
-def generate_ship():
-    verts = [(-1, 0), (-2, 1), (0, 5), (2, 1), (1, 0)]
-    return verts
 
 
 def generate_circle(radius, num_segments):
@@ -24,34 +18,6 @@ def generate_circle(radius, num_segments):
         verts.append((radius * sin(theta), radius * cos(theta)))
     return verts
 
-
-
-class Player(object):
-
-    def __init__(self):
-        verts = generate_ship()
-        block = Block(Material.steel, verts)
-        self.chunks = [Chunk(block)]
-
-
-    def add_to_space(self, space, position, angle):
-        self.chunks[0].add_to_space(space, (position[0]+1, position[1]), angle)
-
-
-    def move(self):
-        body = self.chunks[0].body
-        if Keyboard.keystate[key.RIGHT]:
-            torque = -3000
-        elif Keyboard.keystate[key.LEFT]:
-            torque = +3000
-        else:
-            torque = -body.angular_velocity * 1000
-        body.torque = torque
-
-        if Keyboard.keystate[key.UP]:
-            body.apply_impulse(body.rotation_vector.rotated(90) * 150, (0, 0))
-        elif Keyboard.keystate[key.DOWN]:
-            body.apply_impulse(body.rotation_vector.rotated(-90) * 100, (0, 0))
 
 
 class World(object):
