f19b311891561fc5c1dc0bd9ea8a463eea823fbb
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2009-08-09 23:00:52 +0000

    split SvgParser into SvgParser and Svg Svg knows how to find its own boundary - by path['boundary'] or convex_hull Add some fun shapes back into the used dataset Ditch gravity

--- trunk/solescion/controller/graphics.py
+++ trunk/solescion/controller/graphics.py
@@ -6,10 +6,11 @@ from solescion.geom.svgload.svg_parser import SvgParser
 
 def load_graphics():
     graphics = {}
+    parser = SvgParser()
     for file in listdir('data'):
         if file.endswith('.svg'):
-            svg = SvgParser(join('data', file))
-            svg.parse_svg()
+            print 'file', file
+            svg = parser.parse(join('data', file))
             graphics[file[:-4]] = svg
     return graphics
 
