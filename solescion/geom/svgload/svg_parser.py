9a0a1992e73aab1e31bffb674d6ab4e43d94b089
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2009-08-09 23:00:52 +0000

    split SvgParser into SvgParser and Svg Svg knows how to find its own boundary - by path['boundary'] or convex_hull Add some fun shapes back into the used dataset Ditch gravity

--- solescion/geom/svgload/svg_parser.py
+++ solescion/geom/svgload/svg_parser.py
@@ -1,40 +1,28 @@
 
 import xml.dom.minidom
 
+from shapely.geometry import MultiPoint
+
 from path_parser import PathParser
 
+from solescion.geom.loop import Loop
+from solescion.geom.path import Path
 
-class SvgParser(object):
+
+class Svg(object):
     '''
     Maintains an ordered list of paths, each one corresponding to a path tag
     from an SVG file. Creates a pylget Batch containing all these paths, for
     rendering as a single OpenGL GL_TRIANGLES indexed vert primitive.
     '''
-    def __init__(self, filename=None):
-        '''
-        filename: string, absolute or relative filename of an SVG file
-        '''
-        self.filename = filename
+    def __init__(self):
         self.paths = {}
         self.path_order = []
 
 
-    def parse_svg(self):
-        '''
-        Populates self.paths from the <path> tags in the svg file.
-        '''
-        doc = xml.dom.minidom.parse(self.filename)       
-        path_tags = doc.getElementsByTagName('path')
-        parser = PathParser()
-        for path_tag in path_tags:
-            id, path = parser.parse(path_tag)
-            self.paths[id] = path
-            self.path_order.append(id)
-
-        boundary = self.paths['boundary']
-        x, y = boundary.get_centroid()
-        for path in self.paths.values():
-            path.offset(-x, -y)
+    def add_path(self, id, path):
+        self.paths[id] = path
+        self.path_order.append(id)
 
 
     def add_to_batch(self, batch):
@@ -46,4 +34,46 @@ class SvgParser(object):
         for name in self.path_order:
             path = self.paths[name]
             path.add_to_batch(batch)
+
+
+    def get_boundary(self):
+        if 'boundary' in self.paths:
+            boundary = self.paths['boundary']
+        else:
+            boundary = Path([self.convex_hull()])
+        return boundary
+
+
+    def all_verts(self):
+        for path in self.paths.itervalues():
+            for loop in path.loops:
+                for vert in loop.verts:
+                    yield vert
+
+
+    def convex_hull(self):
+        points = list(self.all_verts())
+        multipoint = MultiPoint(points)
+        hullpoints = [v for v in multipoint.convex_hull.exterior.coords][:-1]
+        return Loop(hullpoints)
+
+
+class SvgParser(object):
+    '''
+    parse(filename) returns an Svg object, populated from the <path> tags
+    in the file.
+    '''
+    def parse(self, filename):
+        svg = Svg()
+        doc = xml.dom.minidom.parse(filename)       
+        path_tags = doc.getElementsByTagName('path')
+        parser = PathParser()
+        for path_tag in path_tags:
+            id, path = parser.parse(path_tag)
+            svg.add_path(id, path)
+
+        x, y = svg.get_boundary().get_centroid()
+        for path in svg.paths.values():
+            path.offset(-x, -y)
+        return svg
 
--- solescion/model/game_ent.py
+++ solescion/model/game_ent.py
@@ -23,7 +23,7 @@ class GameEnt(object):
         self.batch = Batch()
         graphic.add_to_batch(self.batch)
 
-        boundary = graphic.paths['boundary']
+        boundary = graphic.get_boundary()
         self.body = Body(boundary.get_mass(), boundary.get_moment())
         self.shapes = [loop.get_shape(self.body) for loop in boundary.loops]
 
