d416dd4846038bd5614fb895f4b651513254986a
tartley <tartley@d7f58e86-e44f-0410-bf40-57009e94a378>
2009-11-05 00:01:30 +0000

    fix make-virenv.bat

--- trunk/bin/make-virenv.bat
+++ trunk/bin/make-virenv.bat
@@ -1,5 +1,5 @@
 pip -E virenv-%COMPUTERNAME% install pyglet
 pip -E virenv-%COMPUTERNAME% install pymunk
 pip -E virenv-%COMPUTERNAME% install shapely
-%COMPUTERNAME%\Scripts\activate.bat
+virenv-%COMPUTERNAME%\Scripts\activate.bat
 
